3544d003c107eb14da8bed9fb2c03ebf8a8419c2
Tigran Arabadjyan <arabadj@gmail.com>
2020-11-29 14:47:28 +0400

    modify cmd scripts

--- vagrant/cmd/install-descktop-centos8.sh
+++ vagrant/cmd/install-descktop-centos8.sh
@@ -7,6 +7,10 @@
 
 sudo yum --enablerepo=epel,PowerTools group install -y "KDE Plasma Workspaces" "base-x"
 
+sudo yum install -y gdm
+sudo systemctl enable gdm
+sudo systemctl start gdm
+
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 sudo yum localinstall -y google-chrome-stable_current_x86_64.rpm
 sudo rm -f google-chrome-stable_current_x86_64.rpm
