210e1b3b7cb786e32865cd385fa38bd97043eab7
Rafayel Zakoyan <rubomaf@gmail.com>
2020-12-01 16:41:30 +0400

    Delete ping-slack.sh

--- github/workflows/scripts/ping-slack.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' https://hooks.slack.com/services/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
