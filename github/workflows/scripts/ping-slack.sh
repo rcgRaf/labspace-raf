6b4e212965e6367ff40258af9fa10017aa88af80
Rafayel Zakoyan <rubomaf@gmail.com>
2020-12-01 16:39:14 +0400

    Create ping-slack.sh

---
+++ github/workflows/scripts/ping-slack.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' https://hooks.slack.com/services/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
