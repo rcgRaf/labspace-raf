a8a82259d0b2e9f6b77cefd50c78fe756d160c70
Rafayel Zakoyan <rubomaf@gmail.com>
2020-12-01 18:33:32 +0400

    Update ping-slack.sh

--- .github/scripts/ping-slack.sh
+++ .github/scripts/ping-slack.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' https://hooks.slack.com/services/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
+sudo curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' ${{ secrets.SLACK_WEBHOOK }}
