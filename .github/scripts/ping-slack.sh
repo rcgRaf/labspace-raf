9dc8c2b15536d0312123f86be12eeea6c3d87e08
Rafayel Zakoyan <rubomaf@gmail.com>
2020-12-08 20:36:02 +0400

    Update ping-slack.sh

--- .github/scripts/ping-slack.sh
+++ .github/scripts/ping-slack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 data=$(jq .comment.user $GITHUB_EVENT_PATH)
 echo $data
-curl -X POST -H 'Content-type: application/json' -d '{"text": "tvar`", "blocks": '"$data"'}' $ENV_SLACK_WEBHOOK
+curl -X POST -H 'Content-type: application/json' -d '{"text": "tvar`", "blocks": ['"$data"']}' $ENV_SLACK_WEBHOOK
