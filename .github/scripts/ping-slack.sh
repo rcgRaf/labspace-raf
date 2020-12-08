8385171dc78f17cccf073198cb8039b054ebf088
Rafayel Zakoyan <rubomaf@gmail.com>
2020-12-08 16:46:09 +0400

    Update ping-slack.sh

--- .github/scripts/ping-slack.sh
+++ .github/scripts/ping-slack.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-test=$(jq $ENV_MESSAGE)
-echo $test
+echo $ENV_MESSAGE
 sudo curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' $ENV_SLACK_WEBHOOK
