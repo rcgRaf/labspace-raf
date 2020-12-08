27e902c85eb01e05bb554dde4666d1f073ab1498
Rafayel Zakoyan <rubomaf@gmail.com>
2020-12-08 19:36:27 +0400

    Update ping-slack.sh

--- .github/scripts/ping-slack.sh
+++ .github/scripts/ping-slack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 test=$GITHUB_EVENT_PATH
-data=$(jq . $test)
+data=$(jq .action $test)
 echo $test
 echo $data
 cat $GITHUB_EVENT_PATH | curl -X POST -H 'Content-type: application/json' -d @- $ENV_SLACK_WEBHOOK
